6b03aae431ce3d8d3ae00462a5302c596977d1e5
piyush <piyush72717272@gmail.com>
2025-01-25 21:55:31 +0530

    fixes

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class AppColors {
+class XColors {
   static const background = Color.fromRGBO(25, 26, 26, 1);
   static const sideNav = Color.fromRGBO(32, 34, 34, 1);
   static const searchBar = Color.fromRGBO(32, 34, 34, 1);
