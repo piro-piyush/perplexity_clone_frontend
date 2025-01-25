76fe197763870592b9b934fd7bfacd14f79e9ed4
piyush <piyush72717272@gmail.com>
2025-01-25 21:55:55 +0530

    fixes

---
+++ lib/features/perplexity/home/screens/responsive_screens/home_tablet_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeTabletScreen extends StatelessWidget {
+  const HomeTabletScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
