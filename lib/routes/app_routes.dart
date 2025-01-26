d01059b1f1356b2f598fc071de2dc20170cbc811
piyush <piyush72717272@gmail.com>
2025-01-26 21:32:44 +0530

    right side bar

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,5 +1,10 @@
 import 'package:get/get.dart';
+import 'package:perplexity_clone/features/perplexity/chat/screens/chat_screen.dart';
+import 'package:perplexity_clone/features/perplexity/discover/screens/discover_screen.dart';
 import 'package:perplexity_clone/features/perplexity/home/screens/home_screen.dart';
+import 'package:perplexity_clone/features/perplexity/library/screens/library_screen.dart';
+import 'package:perplexity_clone/features/perplexity/spaces/screens/spaces_screen.dart';
+import 'package:perplexity_clone/features/personalization/settings/screens/settings_screen.dart';
 import 'package:perplexity_clone/routes/routes.dart';
 
 class XAppRoute {
@@ -8,8 +13,14 @@ class XAppRoute {
     // GetPage(name: XRoutes.forgetPassword, page: () => const ForgetPasswordScreen()),
     // GetPage(name: XRoutes.resetPassword, page: () => const ResetPasswordScreen()),
     GetPage(name: XRoutes.home, page: () => const HomeScreen()),
-    // GetPage(name: XRoutes.discover, page: () => const DiscoverScreen()),
-    // GetPage(name: XRoutes.spaces, page: () => const SpacesScreen()),
-    // GetPage(name: XRoutes.library, page: () => const LibraryScreen()),
+    GetPage(name: XRoutes.discover, page: () => const DiscoverScreen()),
+    GetPage(name: XRoutes.spaces, page: () => const SpacesScreen()),
+    GetPage(name: XRoutes.library, page: () => const LibraryScreen()),
+
+    // Personalization
+    GetPage(name: XRoutes.settings, page: () => const SettingsScreen()),
+
+    // Chat
+    GetPage(name: XRoutes.chat, page: () => const ChatScreen()),
   ];
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -9,6 +9,12 @@ class XRoutes {
   static const spaces = '/spaces';
   static const library = '/library';
 
+  // Personalization
+  static const settings = '/settings';
+
+  // Chat
+  static const chat = '/chat';
+
   // Side menu items
   static List<String> sideMenuItems = [
     home,
