aba6743032fce18447d7d70ff57c69c907e9f5d8
piyush <piyush72717272@gmail.com>
2025-01-26 21:35:54 +0530

    all fixes of home ui

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,65 @@
+/* -- App Image Strings -- */
+
+/// This class contains all the App Images in String formats.
+class XImages {
+  // -- App Logos
+  static const String darkAppLogo = "assets/logos/app_logo/icon_black.svg";
+  static const String lightAppLogo = "assets/logos/app_logo/icon_white.svg";
+
+  // -- App Headers
+  static const String whiteLogoWithText =
+      "assets/logos/app_headers/black_logo_with_black_text.png";
+  static const String blackLogoWithText =
+      "assets/logos/app_headers/white_logo_with_white_text.png";
+  static const String cyanLogoWithWhiteText =
+      "assets/logos/app_headers/cyan_color_logo_with_white_text.png";
+
+  // -- Default Images
+
+  // -- Social Logos
+  static const String tweeter = "assets/social_icons/tweeter.svg";
+  static const String discord = "assets/social_icons/discord.svg";
+
+  // -- Svg Icons
+  static const String appIcon = "assets/icons/app_icon.svg";
+  static const String ai = "assets/icons/ai.svg";
+  static const String arrowDown = "assets/icons/arrow_down.svg";
+  static const String arrowFortyFiveDegree =
+      "assets/icons/arrow_fourty_five_degree.svg";
+  static const String arrowLeft = "assets/icons/arrow_left.svg";
+  static const String arrowRight = "assets/icons/arrow_right.svg";
+  static const String art = "assets/icons/art.svg";
+  static const String bookmark = "assets/icons/bookmark.svg";
+  static const String close = "assets/icons/close.svg";
+  static const String copy = "assets/icons/copy.svg";
+  static const String discover = "assets/icons/discover.svg";
+  static const String dollar = "assets/icons/dollar.svg";
+  static const String download = "assets/icons/download.svg";
+  static const String entertainment = "assets/icons/entertainment.svg";
+  static const String genImage = "assets/icons/generate_image.svg";
+  static const String home = "assets/icons/home.svg";
+  static const String image = "assets/icons/images.svg";
+  static const String library = "assets/icons/library.svg";
+  static const String person = "assets/icons/person.svg";
+  static const String question = "assets/icons/question.svg";
+  static const String settings = "assets/icons/settings.svg";
+  static const String sources = "assets/icons/sources.svg";
+  static const String spaces = "assets/icons/spaces.svg";
+  static const String sports = "assets/icons/sports.svg";
+  static const String video = "assets/icons/videos.svg";
+
+  //Profile
+  static const String user =
+      "https://shotkit.com/wp-content/uploads/2021/02/black_and-white_portraits_christopher_campbell.jpg";
+
+// // -- Payment Methods
+// static const String applePay = "assets/icons/payment_methods/apple-pay.png";
+// static const String googlePay = "assets/icons/payment_methods/google-pay.png";
+// static const String creditCard = "assets/icons/payment_methods/credit-card.png";
+// static const String masterCard = "assets/icons/payment_methods/master-card.png";
+// static const String paypal = "assets/icons/payment_methods/paypal.png";
+// static const String visa = "assets/icons/payment_methods/visa.png";
+// static const String paystack = "assets/icons/payment_methods/paystack.png";
+// static const String paytm = "assets/icons/payment_methods/paytm.png";
+// static const String successfulPaymentIcon = "assets/icons/payment_methods/successful_payment_icon.png";
+}
