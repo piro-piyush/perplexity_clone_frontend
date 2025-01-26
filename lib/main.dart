ed2e487404a075b708169ee11441baca161750c1
piyush <piyush72717272@gmail.com>
2025-01-26 21:29:55 +0530

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:perplexity_clone/pages/home_page.dart';
-import 'package:perplexity_clone/utils/constants/colors.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+import 'app.dart';
+
+void main() async {
+  // Initialize GetX Local Storage
+  await GetStorage.init();
+
+  // Remove # sign from url
+  setPathUrlStrategy();
+
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Perplexity Clone',
-      theme: ThemeData(
-        scaffoldBackgroundColor: XColors.background,
-        colorScheme: ColorScheme.fromSeed(seedColor: XColors.submitButton),
-        textTheme: GoogleFonts.interTextTheme(
-          ThemeData.dark().textTheme.copyWith(
-                bodyMedium: const TextStyle(
-                  fontSize: 15,
-                  color: XColors.whiteColor,
-                ),
-              ),
-        ),
-      ),
-      home: HomePage(),
-    );
-  }
 }
