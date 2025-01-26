f129a700a64c5c64813bb8b127876fc5b4709352
piyush <piyush72717272@gmail.com>
2025-01-26 21:33:15 +0530

    home screen

--- lib/features/perplexity/home/screens/home_screen.dart
+++ lib/features/perplexity/home/screens/home_screen.dart
@@ -2,16 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:perplexity_clone/common/widgets/templates/site_layout.dart';
 import 'package:perplexity_clone/features/perplexity/home/screens/responsive_screens/home_desktop_screen.dart';
 import 'package:perplexity_clone/features/perplexity/home/screens/responsive_screens/home_mobile_screen.dart';
-import 'package:perplexity_clone/features/perplexity/home/screens/responsive_screens/home_tablet_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const TSiteTemplate(
+    return const XSiteTemplate(
       desktop: HomeDesktopScreen(),
-      tablet: HomeTabletScreen(),
       mobile: HomeMobileScreen(),
     );
   }
--- lib/features/perplexity/home/screens/responsive_screens/home_desktop_screen.dart
+++ lib/features/perplexity/home/screens/responsive_screens/home_desktop_screen.dart
@@ -1,10 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:perplexity_clone/features/perplexity/home/screens/widgets/footer_button.dart';
+import 'package:perplexity_clone/features/perplexity/home/screens/widgets/quick_searches_buttons.dart';
+import 'package:perplexity_clone/services/chat_web_service.dart';
+import 'package:perplexity_clone/widgets/search_section.dart';
 
-class HomeDesktopScreen extends StatelessWidget {
+class HomeDesktopScreen extends StatefulWidget {
   const HomeDesktopScreen({super.key});
 
   @override
+  State<HomeDesktopScreen> createState() => _HomeDesktopScreenState();
+}
+
+class _HomeDesktopScreenState extends State<HomeDesktopScreen> {
+  @override
+  void initState() {
+    super.initState();
+    ChatWebService().connect();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Search Section
+                  SearchSection(),
+
+                  SizedBox(
+                    height: 16,
+                  ),
+
+                  QuickSearchesButton(),
+                ],
+              )),
+
+              // Footer
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: Wrap(
+                  alignment: WrapAlignment.center,
+                  children: [
+                    FooterButton(
+                      title: "Pro",
+                      onTap: () {},
+                    ),
+                    FooterButton(
+                      title: "Enterprise",
+                      onTap: () {},
+                    ),
+                    FooterButton(
+                      title: "Store",
+                      onTap: () {},
+                    ),
+                    FooterButton(
+                      title: "Blog",
+                      onTap: () {},
+                    ),
+                    FooterButton(
+                      title: "Careers",
+                      onTap: () {},
+                    ),
+                    FooterButton(
+                      title: "Education",
+                      onTap: () {},
+                    ),
+                    FooterButton(
+                      title: "English (English) ",
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
