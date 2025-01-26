5b7436a349207f49fff0f79fd4e2a3cae44cc6d1
piyush <piyush72717272@gmail.com>
2025-01-26 21:34:38 +0530

    colors

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
 class XColors {
-  static const background = Color.fromRGBO(25, 26, 26, 1);
-  static const sideNav = Color.fromRGBO(32, 34, 34, 1);
-  static const searchBar = Color.fromRGBO(32, 34, 34, 1);
-  static const searchBarBorder = Color.fromRGBO(60, 63, 64, 1);
-  static const iconGrey = Color(0xFF909090);
-  static const textGrey = Color(0xFFAAAAAA);
-  static const footerGrey = Color(0xFF737373);
-  static const proButton = Color.fromRGBO(47, 48, 47, 1);
-  static const cardColor = Color(0xFF262626);
-  static const submitButton = Color.fromRGBO(27, 185, 206, 1);
-  static const whiteColor = Colors.white;
+  // Dark colors
+  static const background = Color.fromRGBO(25, 26, 26, 1); // Dark background
+  static const sideNav = Color.fromRGBO(32, 34, 34, 1); // Side navigation
+  static const searchBar = Color.fromRGBO(32, 34, 34, 1); // Search bar
+  static const iconBg = Color(0xFF2D2F2F); // Icon background
+  static const proButton = Color.fromRGBO(47, 48, 47, 1); // Pro button
+  static const searchBarBorder = Color.fromRGBO(60, 63, 64, 1); // Search bar border
+  static const cardColor = Color(0xFF262626); // Card color
+
+  // Grey tones
+  static const iconGrey = Color(0xFF909090); // Icon grey
+  static const textGrey = Color(0xFFAAAAAA); // Text grey
+  static const footerGrey = Color(0xFF737373); // Footer grey
+
+  // Accent colors
+  static const activeIconColor = Color(0xFFE8E8E6); // Active icon
+  static const submitButton = Color.fromRGBO(27, 185, 206, 1); // Submit button
+
+  // Light colors
+  static const whiteColor = Colors.white; // White color
 }
