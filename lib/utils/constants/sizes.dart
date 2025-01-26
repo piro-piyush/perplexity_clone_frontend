f2f8b826b7bad92eae47ca804488b22864676ae5
piyush <piyush72717272@gmail.com>
2025-01-26 21:34:48 +0530

    sizes

--- lib/utils/constants/sizes.dart
+++ lib/utils/constants/sizes.dart
@@ -1,4 +1,3 @@
-
 class XSizes {
   // Padding and margin sizes
   static const double xs = 4.0;
@@ -74,6 +73,3 @@ class XSizes {
   static const int mobileScreenSize = 360;
   static const int customScreenSize = 1100;
 }
-
-
-
