c8eac1b47a45da9a5c128bc80c74688de75a8dbb
piyush <piyush72717272@gmail.com>
2025-01-27 22:22:12 +0530

    tablet layout

--- lib/common/widgets/responsive/screens/tablet_layout.dart
+++ lib/common/widgets/responsive/screens/tablet_layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:perplexity_clone/common/widgets/layouts/sidebar/sidebar.dart';
+import 'package:perplexity_clone/utils/constants/colors.dart';
+import 'package:perplexity_clone/utils/constants/sizes.dart';
 
 class TabletLayout extends StatelessWidget {
   TabletLayout({super.key, this.body});
@@ -10,22 +12,26 @@ class TabletLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          // Expanded(child: ),
-          SideBar(),
-          Expanded(
-              // flex: 5,
-              child: Column(
-            children: [
-              // Header
-              // THeader(),
-
-              // Body
-              Expanded(child: body ?? SizedBox()),
-            ],
-          ))
-        ],
+      backgroundColor: XColors.sideNav,
+      body: Padding(
+        padding: EdgeInsets.only(right: 8, top: 8, bottom: 8),
+        child: Row(
+          children: [
+            SideBar(),
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                color: XColors.background,
+                border: Border.all(
+                  color: XColors.iconGrey,
+                  width: 0.1,
+                ),
+                borderRadius: BorderRadius.circular(XSizes.cardRadiusSm),
+              ),
+              child: body ?? SizedBox(),
+            )),
+          ],
+        ),
       ),
     );
   }
