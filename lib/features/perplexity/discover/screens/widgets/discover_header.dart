20065498ecc68301d5a0537894ca7ea062259a02
piyush <piyush72717272@gmail.com>
2025-02-01 21:56:12 +0530

    discover screen

---
+++ lib/features/perplexity/discover/screens/widgets/discover_header.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:perplexity_clone/utils/constants/colors.dart';
+import 'package:perplexity_clone/utils/constants/image_strings.dart';
+
+class DiscoverHeader extends StatefulWidget {
+  const DiscoverHeader({
+    super.key,
+  });
+
+  @override
+  State<DiscoverHeader> createState() => _DiscoverHeaderState();
+}
+
+class _DiscoverHeaderState extends State<DiscoverHeader> {
+  bool isHovering = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border(
+              bottom: BorderSide(color: XColors.searchBarBorder, width: 1))),
+      padding: EdgeInsets.symmetric(horizontal: 162),
+      child: Padding(
+          padding: EdgeInsets.only(left: 32, right: 32, top: 16),
+          child: Container(
+            padding: EdgeInsets.only(bottom: 16),
+            child: Row(
+              spacing: 8,
+              children: [
+                SvgPicture.asset(
+                  XImages.discover,
+                  height: 26,
+                  width: 32,
+                  colorFilter: ColorFilter.mode(
+                    XColors.whiteColor,
+                    BlendMode.srcIn,
+                  ),
+                ),
+                Text(
+                  "Discover",
+                  style: TextStyle(fontSize: 32),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
