528ca2bd0c6c95f8f0ca1640cc19cdc573a656fe
piyush <piyush72717272@gmail.com>
2025-02-01 21:56:09 +0530

    discover screen

--- lib/features/perplexity/discover/screens/responsive_screens/discover_desktop_screen.dart
+++ lib/features/perplexity/discover/screens/responsive_screens/discover_desktop_screen.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:perplexity_clone/common/widgets/icons/question_mark.dart';
+import 'package:perplexity_clone/features/perplexity/discover/controllers/discover_controller.dart';
+import 'package:perplexity_clone/features/perplexity/discover/screens/widgets/discover_header.dart';
+import 'package:perplexity_clone/features/perplexity/discover/screens/widgets/side_card.dart';
 
 class DiscoverDesktopScreen extends StatelessWidget {
   const DiscoverDesktopScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Discover Screen"),
+    final controller = Get.put(DiscoverController());
+    return Stack(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            DiscoverHeader(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 162),
+              child: Row(
+                spacing: 16,
+                children: [
+                  Column(
+                    children: [
+                      // Default tab controller
+                      // DefaultTabController(
+                      //   length: controller.headers.length,
+                      //   child: controller.headers
+                      //       .map((header) =>
+                      //       Container(
+                      //         padding: EdgeInsets.all(16),
+                      //         child:,
+                      //       ))
+                      //       .toList(),),
+
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32),
+                        child: Column(
+                          children: [
+                            // here i want to make
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 64),
+                    child: SideCard(),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+
+        // Question Mark Icon
+        QuestionMark(),
+      ],
     );
   }
 }
