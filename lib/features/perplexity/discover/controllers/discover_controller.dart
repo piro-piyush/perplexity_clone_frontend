4ca4d95c47e4c979e306c976b019ca8a73b6d4cc
piyush <piyush72717272@gmail.com>
2025-02-02 18:58:27 +0530

    discover section

--- lib/features/perplexity/discover/controllers/discover_controller.dart
+++ lib/features/perplexity/discover/controllers/discover_controller.dart
@@ -1,25 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:perplexity_clone/utils/constants/image_strings.dart';
 
-class DiscoverController extends GetxController {
+class DiscoverController extends GetxController
+    with GetSingleTickerProviderStateMixin {
   static DiscoverController get instance => Get.find();
 
-  List<String> headers = [
-    "For You",
-    "Top",
-    "Tech & Science",
-    "Finance",
-    "Arts & Culture",
-    "Sports",
-    "Entertainment"
+  late TabController tabController;
+
+  final List<Map<String, String>> tabs = [
+    {"icon": XImages.forYou, "title": "For You"},
+    {"icon": XImages.top, "title": "Top"},
+    {"icon": XImages.ai, "title": "Tech & Science"},
+    {"icon": XImages.dollar, "title": "Finance"},
+    {"icon": XImages.art, "title": "Arts & Culture"},
+    {"icon": XImages.sports, "title": "Sports"},
+    {"icon": XImages.entertainment, "title": "Entertainment"},
   ];
-  List<String> icons = [
-    XImages.forYou,
-    XImages.top,
-    XImages.ai,
-    XImages.dollar,
-    XImages.art,
-    XImages.sports,
-    XImages.entertainment,
-  ];
+
+  @override
+  void onInit() {
+    super.onInit();
+    tabController = TabController(length: tabs.length, vsync: this);
+  }
+
+  @override
+  void onClose() {
+    tabController.dispose();
+    super.onClose();
+  }
 }
--- lib/features/perplexity/discover/screens/responsive_screens/discover_desktop_screen.dart
+++ lib/features/perplexity/discover/screens/responsive_screens/discover_desktop_screen.dart
@@ -1,58 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:perplexity_clone/common/widgets/icons/question_mark.dart';
-import 'package:perplexity_clone/features/perplexity/discover/controllers/discover_controller.dart';
 import 'package:perplexity_clone/features/perplexity/discover/screens/widgets/discover_header.dart';
 import 'package:perplexity_clone/features/perplexity/discover/screens/widgets/side_card.dart';
+import 'package:perplexity_clone/features/perplexity/discover/screens/widgets/tab_bar_child.dart';
+import 'package:perplexity_clone/features/perplexity/discover/screens/widgets/tab_bar_widget.dart';
 
 class DiscoverDesktopScreen extends StatelessWidget {
   const DiscoverDesktopScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.put(DiscoverController());
     return Stack(
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DiscoverHeader(),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 162),
-              child: Row(
-                spacing: 16,
-                children: [
-                  Column(
-                    children: [
-                      // Default tab controller
-                      // DefaultTabController(
-                      //   length: controller.headers.length,
-                      //   child: controller.headers
-                      //       .map((header) =>
-                      //       Container(
-                      //         padding: EdgeInsets.all(16),
-                      //         child:,
-                      //       ))
-                      //       .toList(),),
-
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 32),
-                        child: Column(
-                          children: [
-                            // here i want to make
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 64),
-                    child: SideCard(),
-                  )
-                ],
+        SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DiscoverHeader(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 162),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 16,
+                  children: [
+                    Stack(
+                        children: [
+                      SizedBox(
+                          width: 674,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 64.0),
+                            // Tab Bar child is very long i want that it can scroll
+                            child: TabBarChild(),
+                          )),
+                      // **Tab Bar (Fixed Height, No Expanded)**
+                      TabBarWidget(),
+                    ]),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 64),
+                      child: SideCard(),
+                    ),
+                  ],
+                ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
 
         // Question Mark Icon
