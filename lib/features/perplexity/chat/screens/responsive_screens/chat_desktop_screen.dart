a4780f5bece1c3945564a673109ea6b49f319c7b
piyush <piyush72717272@gmail.com>
2025-02-02 21:59:59 +0530

    chat feature

--- lib/features/perplexity/chat/screens/responsive_screens/chat_desktop_screen.dart
+++ lib/features/perplexity/chat/screens/responsive_screens/chat_desktop_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:perplexity_clone/common/widgets/icons/question_mark.dart';
 import 'package:perplexity_clone/features/perplexity/chat/screens/widgets/answer_body.dart';
 import 'package:perplexity_clone/features/perplexity/chat/screens/widgets/answer_header.dart';
 import 'package:perplexity_clone/features/perplexity/chat/screens/widgets/side_section_chat.dart';
@@ -13,26 +14,41 @@ class ChatDesktopScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AnswerHeader(),
-            Padding(
-              padding: const EdgeInsets.only(left: 32, right: 32, top: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                spacing: 16,
-                children: [
-                  AnswerBody(question: question),
-                  XDeviceUtils.isDesktopScreen(context)
-                      ? SideSectionChat()
-                      : SizedBox(),
-                ],
+        SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AnswerHeader(
+                header: question,
               ),
-            ),
-          ],
-        )
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 16, right: 16, top: 16, bottom: 96),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 16,
+                  children: [
+                    Expanded(flex: 3, child: AnswerBody(question: question)),
+                    Expanded(
+                      child: XDeviceUtils.isDesktopScreen(context)
+                          ? SideSectionChat()
+                          : SizedBox(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        // Question Mark Icon
+        QuestionMark(),
       ],
     );
   }
 }
+
+
+
