691feef8748a5976db101bf5f194d54ca2801d48
piyush <piyush72717272@gmail.com>
2025-02-05 22:08:07 +0530

    Refactor: Improve Library Screen layout and conditional rendering
    - Removed unnecessary widgets and reduced nesting.
    - Implemented a centered layout for the desktop version with a fixed width.
    - Added conditional rendering based on login status.

--- lib/features/perplexity/library/screens/library_screen.dart
+++ lib/features/perplexity/library/screens/library_screen.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:perplexity_clone/common/widgets/login/login_suggestion.dart';
 import 'package:perplexity_clone/common/widgets/templates/site_layout.dart';
 import 'package:perplexity_clone/features/perplexity/library/screens/responsive_screens/library_desktop_screen.dart';
 import 'package:perplexity_clone/features/perplexity/library/screens/responsive_screens/library_mobile_screen.dart';
+import 'package:perplexity_clone/features/perplexity/library/screens/widgets/header.dart';
 
 class LibraryScreen extends StatelessWidget {
   const LibraryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return XSiteTemplate(
-      desktop: LibraryDesktopScreen(),
-      mobile: LibraryMobileScreen(),
-    );
+    bool isLogin = true;
+    return isLogin
+        ? XSiteTemplate(
+      header: LibraryHeader(),
+            desktop: LibraryDesktopScreen(),
+            mobile: LibraryMobileScreen(),
+          )
+        : LoginSuggestion(
+            headerText: "Organize your search history by signing up",
+          );
   }
 }
