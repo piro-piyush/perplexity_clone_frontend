e162ea8123b5c58d48ba86047f4743b5a1b7d8fe
piyush <piyush72717272@gmail.com>
2025-02-05 22:05:36 +0530

    app routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:perplexity_clone/features/auth/login/screens/login_screen.dart';
 import 'package:perplexity_clone/features/perplexity/chat/screens/chat_screen.dart';
 import 'package:perplexity_clone/features/perplexity/discover/screens/discover_screen.dart';
 import 'package:perplexity_clone/features/perplexity/home/screens/home_screen.dart';
@@ -9,9 +10,10 @@ import 'package:perplexity_clone/routes/routes.dart';
 
 class XAppRoute {
   static final List<GetPage> pages = [
-    // GetPage(name: XRoutes.login, page: () => const LoginScreen()),
     // GetPage(name: XRoutes.forgetPassword, page: () => const ForgetPasswordScreen()),
     // GetPage(name: XRoutes.resetPassword, page: () => const ResetPasswordScreen()),
+
+    GetPage(name: XRoutes.login, page: () => const LoginScreen()),
     GetPage(name: XRoutes.home, page: () => const HomeScreen()),
     GetPage(name: XRoutes.discover, page: () => const DiscoverScreen()),
     GetPage(name: XRoutes.spaces, page: () => const SpacesScreen()),
