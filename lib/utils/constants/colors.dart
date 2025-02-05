009e32560f2e4b518badce6a24cb49cce3c16301
piyush <piyush72717272@gmail.com>
2025-02-05 22:05:51 +0530

    color file

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -7,8 +7,10 @@ class XColors {
   static const searchBar = Color.fromRGBO(32, 34, 34, 1); // Search bar
   static const iconBg = Color(0xFF2D2F2F); // Icon background
   static const proButton = Color.fromRGBO(47, 48, 47, 1); // Pro button
-  static const searchBarBorder = Color.fromRGBO(60, 63, 64, 1); // Search bar border
+  static const searchBarBorder =
+      Color.fromRGBO(60, 63, 64, 1); // Search bar border
   static const cardColor = Color(0xFF262626); // Card color
+  static const textFieldColor = Color(0xFF202222); // Card color
 
   // Grey tones
   static const iconGrey = Color(0xFF909090); // Icon grey
@@ -18,6 +20,9 @@ class XColors {
   // Accent colors
   static const activeIconColor = Color(0xFFE8E8E6); // Active icon
   static const submitButton = Color.fromRGBO(27, 185, 206, 1); // Submit button
+
+  // Login Hover color
+  static const loginHoverColor = Color(0xFF13343b);
 
   // Light colors
   static const whiteColor = Colors.white; // White color
