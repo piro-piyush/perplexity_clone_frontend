71c67df369323d5947347963b5f728637b0fbfbd
piyush <piyush72717272@gmail.com>
2025-02-06 15:34:49 +0530

    refactor: improve big news card layout and image handling
    The big news card widget has been refactored to enhance its layout and image handling:
    
    -   Introduced `AspectRatio` to manage the card's proportions, set to 4/3.
    -   Adjusted the main container's `DecorationImage` to directly utilize `CachedNetworkImageProvider` for improved image loading and management, with `fit: BoxFit.fitHeight`
    -   Modified the bottom overlay container to have a fixed height of 151.
    -   Updated padding to be more specific, enhancing text layout.
    -   Reduced the font size for the title text to 18 and increased the content font size to 16 for better readability.
    -   The curated by row made wrap content.

--- lib/features/perplexity/discover/screens/widgets/big_news_card.dart
+++ lib/features/perplexity/discover/screens/widgets/big_news_card.dart
@@ -17,112 +17,116 @@ class BigNewsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Skeleton.leaf(
-      child: Container(
-        decoration: BoxDecoration(
-            color: XColors.searchBar, borderRadius: BorderRadius.circular(8)),
-        child: Stack(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: CachedNetworkImage(
-                imageUrl: model.imageUrl,
-                fit: BoxFit.contain,
-                placeholder: (context, url) =>
-                    Center(child: CircularProgressIndicator()),
-                // Placeholder while loading
-                errorWidget: (context, url, error) =>
-                    Icon(Icons.error), // Error widget in case of a failure
+      child: AspectRatio(
+        aspectRatio: 4 / 3,
+        child: Container(
+          decoration: BoxDecoration(
+            color: XColors.searchBar,
+            borderRadius: BorderRadius.circular(8),
+            image: DecorationImage(
+              image: CachedNetworkImageProvider(
+                model.imageUrl,
               ),
+              fit: BoxFit.fitHeight,
             ),
-            Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: XColors.searchBar,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(8),
-                      bottomRight: Radius.circular(8),
-                    ),
-                    border: Border.all(
-                        color: XColors.searchBarBorder, width: 0.5)),
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(model.title,
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  height: 151,
+                  decoration: BoxDecoration(
+                      color: XColors.searchBar,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(8),
+                        bottomRight: Radius.circular(8),
+                      ),
+                      border: Border.all(
+                          color: XColors.searchBarBorder, width: 0.5)),
+                  padding:
+                      EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(model.title,
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: XColors.whiteColor,
+                          )),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        model.content,
                         style: TextStyle(
-                          fontSize: 20,
-                          color: XColors.whiteColor,
-                        )),
-                    SizedBox(
-                      height: 2,
-                    ),
-                    Text(
-                      model.content,
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: XColors.iconGrey,
+                          fontSize: 16,
+                          color: XColors.iconGrey,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 8.0,
+                      SizedBox(
+                        height: 8,
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            spacing: 4,
-                            children: [
-                              Container(
-                                width: 20,
-                                height: 20,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    image: DecorationImage(
-                                        image: NetworkImage(
-                                            model.curatedByImage))),
-                              ),
-                              Text(
-                                model.curatedBy,
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  color: XColors.iconGrey,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Container(
-                            width: 32,
-                            height: 32,
-                            decoration: BoxDecoration(),
-                            child: Center(
-                              child: SvgPicture.asset(
-                                XImages.bookmark,
-                                width: 17,
-                                height: 14,
-                                colorFilter: ColorFilter.mode(
-                                  XColors.iconGrey,
-                                  BlendMode.srcIn,
-                                ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 8.0,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Row(
+                                spacing: 4,
+                                children: [
+                                  Container(
+                                    width: 20,
+                                    height: 20,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        image: DecorationImage(
+                                            image: NetworkImage(
+                                                model.curatedByImage))),
+                                  ),
+                                  Text(
+                                    model.curatedBy,
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: XColors.iconGrey,
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
+                            Container(
+                              width: 32,
+                              height: 32,
+                              decoration: BoxDecoration(),
+                              child: Center(
+                                child: SvgPicture.asset(
+                                  XImages.bookmark,
+                                  width: 17,
+                                  height: 14,
+                                  colorFilter: ColorFilter.mode(
+                                    XColors.iconGrey,
+                                    BlendMode.srcIn,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
